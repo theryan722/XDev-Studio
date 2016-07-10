a3889f58e45efbef6f8dc93e9529f11c46017559
Ryan O'Day <romanguy722@gmail.com>
2016-07-10 00:01:16 -0400

    Update assembly info

--- XDev.Path/My Project/AssemblyInfo.vb
+++ XDev.Path/My Project/AssemblyInfo.vb
@@ -10,9 +10,9 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("XDev.Path")> 
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("BioNetWorks Corp.")> 
+<Assembly: AssemblyCompany("Ryan O'Day")> 
 <Assembly: AssemblyProduct("XDev.Path")> 
-<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
